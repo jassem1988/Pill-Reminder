8ba19ff1360ac08c5ed7b9cf100feb035e2e65f9
Jassem Al-Buloushi <jassem1988@gmail.com>
2020-03-03 11:09:06 +0300

    Initial Commit

---
+++ Pill Reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pill Reminder
+//
+//  Created by Jassem Al-Buloushi on 3/3/20.
+//  Copyright © 2020 Jassem Al-Buloushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
