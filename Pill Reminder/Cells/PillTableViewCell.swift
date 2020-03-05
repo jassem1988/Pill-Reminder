90a502502c6ffd0a079cd14e333ceea91a6d8a8c
Jassem Al-Buloushi <jassem1988@gmail.com>
2020-03-05 09:03:48 +0300

    - Create PillCell and register it with Nib - Make the app just Portrait

---
+++ Pill Reminder/Cells/PillTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  PillTableViewCell.swift
+//  Pill Reminder
+//
+//  Created by Jassem Al-Buloushi on 3/5/20.
+//  Copyright © 2020 Jassem Al-Buloushi. All rights reserved.
+//
+
+import UIKit
+
+class PillTableViewCell: UITableViewCell {
+    
+    //MARK:- Outlets
+    @IBOutlet var pillImageView: UIImageView!
+    @IBOutlet var pillNameCell: UILabel!
+    @IBOutlet var instructionsCell: UILabel!
+    @IBOutlet var pillTimerCell: UILabel!
+    @IBOutlet var doneImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Pill Reminder/Controllers/HomeViewController.swift
+++ Pill Reminder/Controllers/HomeViewController.swift
@@ -13,9 +13,16 @@ class HomeViewController: UITableViewController {
     //Properties
     
     let items = [Int](1...10)
+    
+    var allPillNames = [String]()
+    var allPillDoses = [Int]()
+    var allPillTypes = ["Taps", "Pills", "g", "mg", "mcg"]
+    var allPillInstructions = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.registerCell()
         
     }
     
@@ -27,14 +34,25 @@ class HomeViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ReminderCell", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ReminderCell", for: indexPath) as? PillTableViewCell else {
+            fatalError("Could not register cell with identifier ReminderCell")
+        }
         
-        cell.textLabel?.text = String(items[indexPath.row])
+//        cell.textLabel?.text = String(items[indexPath.row])
+        
+        cell.pillNameCell.text = String(items[indexPath.row])
+        
         
         return cell
     }
     
     
+    //MARK:- PillCell own methods
+    
+    func registerCell() {
+        let pillCell = UINib(nibName: "PillTableViewCell", bundle: nil)
+        self.tableView.register(pillCell, forCellReuseIdentifier: "ReminderCell")
+    }
 
 
 }
